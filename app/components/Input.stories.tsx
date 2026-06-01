75d526c2151115be1dfb3c7ef3753c88f039b9f7
alikhandesign <30875221+alikhandesign@users.noreply.github.com>
2026-06-01 18:13:27 -0500

    storybook: add autodocs tag to Input.stories.tsx

--- app/components/Input.stories.tsx
+++ app/components/Input.stories.tsx
@@ -3,6 +3,7 @@ import Input from './Input'
 
 const meta: Meta<typeof Input> = {
   title: 'Inputs/Input',
+  tags: ['autodocs'],
   component: Input,
   argTypes: {
     placeholder: { control: 'text' },
